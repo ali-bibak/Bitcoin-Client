8408ed218264f7c1e0c4066f21815f3f696f21a6
Ali Bibak <bibak.ali@gmail.com>
2020-02-13 05:06:33 -0600

    finished block.rs

--- src/block.rs
+++ src/block.rs
@@ -1,9 +1,12 @@
+extern crate rand;
+
 use serde::{Serialize, Deserialize};
 use ring::digest::{SHA256, digest};
 use std::time::{SystemTime};
 use crate::crypto::hash::{H256, Hashable};
-use crate::crypto::merkle::{MerkleNode, MerkleTree};
+use crate::crypto::merkle::{MerkleTree};
 use crate::transaction::{Transaction};
+use rand::Rng;
 
 #[derive(Serialize, Deserialize, Debug)]
 pub struct Block {
@@ -13,17 +16,16 @@ pub struct Block {
 
 #[derive(Serialize, Deserialize, Debug)]
 pub struct Header {
-    parent: Box<Block>,
-    parent_hash: H256,
+    parent: H256,
     nonce: u32,
     difficulty: H256,
     timestamp: SystemTime,
-    merkle_root: MerkleNode,
 }
 
 #[derive(Serialize, Deserialize, Debug)]
 pub struct Content {
     transactions: Vec<Transaction>,
+    merkle_root: H256,
 }
 
 impl Hashable for Header {
@@ -47,6 +49,37 @@ pub mod test {
     use crate::crypto::hash::H256;
 
     pub fn generate_random_block(parent: &H256) -> Block {
-        unimplemented!()
+        let mut rng = rand::thread_rng();
+        let nonce: u32 = rng.gen();
+
+        let mut bytes32 = [255u8;32];
+        bytes32[0]=0;
+        bytes32[1]=0;
+        bytes32[4]=6;
+        bytes32[10]=13;
+        bytes32[18]=66;
+        bytes32[23]=23;
+        bytes32[27]=41;
+        let difficulty: H256 = bytes32.into();
+
+        let timestamp = SystemTime::now();
+        let header = Header {
+            parent: parent.clone(),
+            nonce,
+            difficulty,
+            timestamp,
+        };
+        let transactions: Vec<Transaction> = Vec::new();
+        let merkle_tree = MerkleTree::new(&transactions);
+        let merkle_root = merkle_tree.root();
+        let content = Content {
+            transactions,
+            merkle_root,
+        };
+        let block = Block {
+            header,
+            content,
+        };
+        return block;
     }
 }
